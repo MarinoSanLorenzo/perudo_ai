261045ebdbc11e07e09a1e484deff82c755ccdb2
Marino <msanlore@gmail.com>
2022-11-28 22:34:30 +0100

    left neighboor

--- tests/units/test_decision.py
+++ tests/units/test_decision.py
@@ -1,4 +1,24 @@
 import pytest
-from perudo_ai.decision import Decision
+from perudo_ai.decision import Decision, Raise
 from perudo_ai.player import Player
 from typing import *
+
+class TestDecision:
+
+    def test_decision_raise(self) -> None:
+        decision = Decision(raise_=Raise(6, 'PACO'))
+        assert isinstance(decision.raise_, Raise)
+        assert decision.equal is False
+        assert decision.bluff is False
+
+    def test_decision_equal(self) -> None:
+        decision = Decision(equal=True)
+        assert decision.raise_ is None
+        assert decision.equal is True
+        assert decision.bluff is False
+
+    def test_decision_bluff(self) -> None:
+        decision = Decision(bluff=True)
+        assert decision.raise_ is None
+        assert decision.equal is False
+        assert decision.bluff is True
