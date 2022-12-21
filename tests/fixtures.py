86e13ed22d9ef23057f5271cf5b742dab7c61afc
Marino <msanlore@gmail.com>
2022-12-21 01:45:57 +0100

    test AI take optimal decision

---
+++ tests/fixtures.py
@@ -0,0 +1,40 @@
+from perudo_ai.game import Game
+import pytest
+from typing import *
+from constants import *
+from perudo_ai.player import Player
+
+__all__ = ["three_players", "dices_lst", "game"]
+
+# three_players =  [Player(name="Marc"), Player("Jean"), Player("Luc")]
+# dices_lst = [
+#         ["2", "2", PACO, PACO, "5"],
+#         ["3", "3", PACO, PACO, "6"],
+#         ["2", "2", PACO, PACO, "5"],
+#     ]
+# game = Game(three_players)
+# for player, dices in zip(game.players.values(), dices_lst):
+#     player._dices = dices
+#
+
+
+@pytest.fixture
+def three_players() -> List[Player]:
+    return [Player(name="Marc"), Player("Jean"), Player("Luc")]
+
+
+@pytest.fixture
+def dices_lst() -> List[List[str]]:
+    return [
+        ["2", "2", PACO, PACO, "5"],
+        ["3", "3", PACO, PACO, "6"],
+        ["2", "2", PACO, PACO, "5"],
+    ]
+
+
+@pytest.fixture()
+def game(three_players: List[Player], dices_lst: List[List[str]]) -> Game:
+    game = Game(three_players)
+    for player, dices in zip(game.players.values(), dices_lst):
+        player._dices = dices
+    return game
